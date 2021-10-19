86fb221a22459d82e16aa70f94441630f4863b8f
J. Logan Affleck <affleck.logan@gmail.com>
2021-10-19 00:10:06 -0600

    prepped about and styles

--- pages/What.js
+++ pages/What.js
@@ -6,17 +6,25 @@ export default function What() {
        <div className='what'>
            <h2>what we do</h2>
 
+           <div className='whatContainer'>
+           <div className='whatText'>
            <h3>design</h3>
            <p>Quisque in augue. Donec aliquam magna nonummy enim. Proin blandit imperdiet sem. Donec malesuada, urna sit amet varius aliquam, nibh tortor laoreet turpis, eget sodales felis  nibh ac sapien. Fusce eget augue. Integer sed risus. Aenean mollis. Donec facilisis egestas quam. Duis bibendum augue  id mauris. Sed laoreet, tortor vel cursus fringilla, turpis elit vestibulum arcu, eu varius dolor leo in nulla. In sem ipsum, faucibus quis, varius tristique, porta eget, lorem. Curabitur hendrerit diam et mauris. Etiam porta nunc euismod dui. </p>
 
            <h3>marketing</h3>
            <p>Quisque in augue. Donec aliquam magna nonummy enim. Proin blandit imperdiet sem. Donec malesuada, urna sit amet varius aliquam, nibh tortor laoreet turpis, eget sodales felis  nibh ac sapien. Fusce eget </p>
+           </div>
 
-           <div>
+           <div className='whatLinks'>
+               <div className='portfolioLink'>
                <h3>portfolio</h3>
+               </div>
+               <div className='reviewsLink'>
                <h3>reviews</h3>
+               </div>
            
            </div>
+           </div> 
 
        </div>
 
--- pages/Who.js
+++ pages/Who.js
@@ -1,11 +1,38 @@
 import Image from 'next/image'
 import About from './About';
 
+let logan = { 
+    src: '/../public/logan.jpg',
+    alt: 'a picture of logan affleck',
+    staffName: 'j. logan affleck',
+    description: 'Logan is a visual designer and fullstack developer. Additionally, he has shown excellence in customer communications and management of high-risk customer-facing scenarios through his Stevie Award-winning work at Google Fiber, Vacasa, and Builder.io.',
+    socials: {
+        github: 'https://github.com/LoganAffleck',
+        linkedin: 'https://www.linkedin.com/in/j-logan-affleck/',
+        twitter: 'https://twitter.com/loganwaffleck',
+        twitch: 'https://www.twitch.tv/loganwaffle',
+        instagram:'https://www.instagram.com/affleck.logan/'
+    }
+}
+
+let anjuli = { 
+    src: '/../public/anjuli.jpg',
+    alt: 'a picture of anjuli waybright',
+    staffName: 'anjuli waybright',
+    description: 'Anjuli is a business strategist and social media marketer. She has managed government and luxury experiences, and loves to build and maintain online presence. Anjuli ',
+    socials: {
+        linkedin: '',
+        twitter: '',
+        instagram:'',
+        facebook: ''
+    }
+}
+
 
 export default function Who() {
     return (
        <div className='who'>
-           <h2>who we are</h2>
+           <h2 className='whoTitle'>who we are</h2>
            <div className='abouts'>
            <div className='aboutContainer'>
            <About src={'/../public/logan.jpg'} alt={'logan'} name={'j.logan affleck'} description={'bolalsdglk'} socials={'eyy'}/>
